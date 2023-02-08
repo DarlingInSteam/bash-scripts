550da5b34745249137a544d155e134c200138bb7
Gerald Alesander <geraldal@cl-a3.nsk.21-school.ru>
2023-02-08 14:04:06 +0700

    fix edit.sh and fix log_analyzer.sh

--- src/edit.sh
+++ src/edit.sh
@@ -6,16 +6,18 @@ read string_for_replace
 echo "Введите строку, на которую хотите изменить(В виде текста)"
 read string_input
 
-if [[ "" != $file_for_edit ]] || [[ "" != $string_for_replace ]] || [[ "" != $string_input ]]; then
+if [[ "" == $file_for_edit ]] || [[ "" == $string_for_replace ]] || [[ "" == $string_input ]]; then
+echo "У вас есть пустая(ые) строка(и)"
+exit 0
+fi
+
+if [[ ! -e $file_for_edit ]]; then
+echo "Файла не существует"
+exit 0
+fi
 
 sed -i "" s/$string_for_replace/$string_input/g $file_for_edit
 
 buf=($(shasum -a 256 $file_for_edit))
 
 echo "$file_for_edit - $(stat -f%z $file_for_edit) - $(date -r $file_for_edit +"%Y-%m-%d %H:%M") - $buf - sha256" >> files.log
-
-else 
-
-echo "У вас есть пустая(ые) строка(и)"
-
-fi
--- src/log_analyzer.sh
+++ src/log_analyzer.sh
@@ -1,23 +1,18 @@
 echo "Введите путь до файла(лога)"
 read path_to_log
 
-current_file_name=""
+file_name=("")
+filtered_names=("")
 current_sh=""
-count_name=0
+
 count_sh=0
 count_lines=0
 
 while IFS= read -r p; do
     IFC='-' read -a strarr <<< $p 
 	((count_lines+=1))
-    
-    if (($count_name==0)); then 
-    current_file_name=${strarr[0]}
-    ((count_name+=1))
-    elif [[ $current_file_name != ${strarr[0]} ]]; then
-    current_file_name=${strarr[0]}
-    ((count_name+=1))
-    fi
+
+    file_name+=(${strarr[0]})
 
     if [[ "NULL" == ${strarr[7]} ]]; then
     continue
@@ -32,8 +27,15 @@ while IFS= read -r p; do
 done < $path_to_log
 
 echo "Количество записей:"
-echo $count_lines
+echo $count_lines   
+echo ${file_name[@]}
+#filtered_names=$($file_name | uniq)
+filtered_names=($(for name in ${file_name[@]}; do
+    echo $name
+done | uniq))
+count_names=${#filtered_names[@]}
+
 echo "Количество уникальных файлов"
-echo $count_name
+echo $count_names
 echo "Количество изменений хэшей"
 echo $count_sh
